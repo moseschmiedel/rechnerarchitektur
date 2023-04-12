1e8ceff10b111cc72425f3cb3e1aed4b39a96b03
Mose Schmiedel <mose@schmiednet.de>
2023-04-12 10:51:34 +0300

    Use inline stack and replace all indexing by pointer arithmetic

--- quick_sort.c
+++ quick_sort.c
@@ -178,43 +178,29 @@ uint64_t __compute_timediff(struct timespec *start, struct timespec *end,
  * ************************************************** */
 
 struct range {
-    size_t low;
-    size_t high;
+    char *low;
+    char *high;
 };
 
-void stack_push(struct range **top, struct range push_elem) {
-    **top = push_elem;
-    ++*top;
-}
-
-struct range stack_pop(struct range **top, const struct range *base) {
-    if (*top == base) {
-        struct range pop_elem;
-        pop_elem.low = 0;
-        pop_elem.high = 0;
-        return pop_elem;
-    }
-    return *--*top;
-}
-
-_Bool stack_empty(const struct range *top, const struct range *base) {
-    return (top - base) <= 0;
-}
+#define PUSH(lo, hi) ((void)((top->low = (lo)), (top->high = (hi)), ++top))
+#define POP(lo, hi) ((void)(--top, (lo = top->low), (hi = top->high)))
+#define STACK_EMPTY ((top - base) <= 0)
 
 void quick_sort(void *array, size_t array_length, size_t elem_size,
                 __compar_fn_t compare_f) {
+    if (array_length <= 1)
+        return;
+
     char *array_base = (char *)array;
 
     // stack initializiation
-    struct range stack_base[array_length];
-    struct range *stack_top = &stack_base[0];
-
-    struct range r;
-    r.low = 0;
-    r.high = array_length - 1;
+    struct range base[array_length];
+    struct range *top = &base[0];
 
     char *pivot;
-    char *i, *j, *lo, *hi;
+    char *i, *j, *mid;
+    char *lo = array_base;
+    char *hi = array_base + elem_size * (array_length - 1);
 
     char temp[elem_size];
 
@@ -222,86 +208,90 @@ void quick_sort(void *array, size_t array_length, size_t elem_size,
     bool scanning = true;
 
     while (sorting) {
-        lo = &array_base[r.low * elem_size];
-        hi = &array_base[r.high * elem_size];
-
-        // Median-of-Three pivot choice
-        // A[l + 1] :=: A[(l + r) / 2]
-        memcpy(temp, &array_base[(r.low + r.high) / 2 * elem_size], elem_size);
-        memmove(&array_base[(r.low + r.high) / 2 * elem_size],
-                &array_base[(r.low + 1) * elem_size], elem_size);
-        memcpy(&array_base[(r.low + 1) * elem_size], temp, elem_size);
-
-        // if A[l + 1] > A[r] then A [l + 1] :=: A[r] endif;
-        if (compare_f(lo + elem_size, hi) > 0) {
-            memcpy(temp, lo + elem_size, elem_size);
-            memmove(lo + elem_size, hi, elem_size);
-            memcpy(hi, temp, elem_size);
-        }
-        // if A[l] > A[r] then A[l] :=: A[r] endif;
-        if (compare_f(lo, hi) > 0) {
-            memcpy(temp, lo, elem_size);
-            memmove(lo, hi, elem_size);
-            memcpy(hi, temp, elem_size);
-        }
-        // if A[l + 1] > A[1] then A[l + 1] :=: A[l] endif;
-        if (compare_f(lo + elem_size, lo) > 0) {
-            memcpy(temp, lo + elem_size, elem_size);
-            memcpy(lo + elem_size, lo, elem_size);
-            memcpy(lo, temp, elem_size);
-        }
-
-        pivot = lo;
-        i = lo;
-        j = hi + elem_size;
-
-        // Partition
-        scanning = true;
-        while (scanning) {
-            do {
-                i += elem_size;
-            } while (i < hi && compare_f(i, pivot) < 0);
-            do {
-                j -= elem_size;
-            } while (compare_f(j, pivot) > 0);
-
-            if (j >= i) {
-                memcpy(temp, i, elem_size);
-                memmove(i, j, elem_size);
-                memcpy(j, temp, elem_size);
-            } else {
-                scanning = false;
+        if (hi - lo <= elem_size) {
+            if (compare_f(lo, hi) > 0) {
+                memcpy(temp, lo, elem_size);
+                memmove(lo, hi, elem_size);
+                memcpy(hi, temp, elem_size);
             }
-        };
-        memcpy(temp, j, elem_size);
-        memmove(j, pivot, elem_size);
-        memcpy(pivot, temp, elem_size);
+            j = lo;
+            i = hi;
+        } else {
+
+            // Median-of-Three pivot choice
+            // A[l + 1] :=: A[(l + r) / 2]
+            mid = lo + ((hi - lo) / elem_size >> 1) * elem_size;
+            memcpy(temp, mid, elem_size);
+            memmove(mid, lo + elem_size, elem_size);
+            memcpy(lo + elem_size, temp, elem_size);
+
+            // if A[l + 1] > A[r] then A [l + 1] :=: A[r] endif;
+            if (compare_f(lo + elem_size, hi) > 0) {
+                memcpy(temp, lo + elem_size, elem_size);
+                memmove(lo + elem_size, hi, elem_size);
+                memcpy(hi, temp, elem_size);
+            }
+            // if A[l] > A[r] then A[l] :=: A[r] endif;
+            if (compare_f(lo, hi) > 0) {
+                memcpy(temp, lo, elem_size);
+                memmove(lo, hi, elem_size);
+                memcpy(hi, temp, elem_size);
+            }
+            // if A[l + 1] > A[1] then A[l + 1] :=: A[l] endif;
+            if (compare_f(lo + elem_size, lo) > 0) {
+                memcpy(temp, lo + elem_size, elem_size);
+                memcpy(lo + elem_size, lo, elem_size);
+                memcpy(lo, temp, elem_size);
+            }
+
+            pivot = lo;
+            i = lo;
+            j = hi + elem_size;
+
+            // Partition
+            scanning = true;
+            while (scanning) {
+                do {
+                    i += elem_size;
+                } while (i < hi && compare_f(i, pivot) < 0);
+                do {
+                    j -= elem_size;
+                } while (compare_f(j, pivot) > 0);
+
+                if (j >= i) {
+                    memcpy(temp, i, elem_size);
+                    memmove(i, j, elem_size);
+                    memcpy(j, temp, elem_size);
+                } else {
+                    scanning = false;
+                }
+            };
+            memcpy(temp, j, elem_size);
+            memmove(j, pivot, elem_size);
+            memcpy(pivot, temp, elem_size);
+        }
 
         // Stop case and Stack manipulation
         if (MAX(j - lo, hi - i + elem_size) <= elem_size) {
-            if (stack_empty(stack_top, stack_base)) {
+            if (STACK_EMPTY) {
                 sorting = false;
             } else {
-                r = stack_pop(&stack_top, stack_base);
+                POP(lo, hi);
             }
         } else if (MIN(j - lo, hi - i + elem_size) <= elem_size) {
             if (j - lo > hi - i + elem_size) {
-                r.high = (j - array_base) / elem_size;
+                hi = j - elem_size;
             } else {
-                r.low = (i - array_base) / elem_size - 1;
+                lo = i;
             }
         } else {
             if (j - lo > hi - i + elem_size) {
-                stack_push(
-                    &stack_top,
-                    (struct range){.low = r.low,
-                                   .high = (j - array_base) / elem_size - 1});
-                r.low = (i - array_base) / elem_size;
+
+                PUSH(lo, (j - elem_size));
+                lo = i;
             } else {
-                stack_push(&stack_top,
-                           (struct range){.low = (i - array_base) / elem_size,
-                                          .high = r.high});
-                r.high = (j - array_base) / elem_size - 1;
+                PUSH(i, hi);
+                hi = j - elem_size;
             }
         }
     }
