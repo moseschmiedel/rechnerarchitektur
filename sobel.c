6043c530d15e8d66ebdeb7660f7cfc8c917204e3
Mose Schmiedel <mose@schmiednet.de>
2023-05-15 16:13:34 +0200

    Try to add binary file handling to sobel

--- sobel.c
+++ sobel.c
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "eisvogel.h"
-
 #define ENABLE_LOCAL_MAIN
 #define DEBUG
 
@@ -11,12 +9,13 @@
 #include <stdio.h>
 #endif
 
-#define RED(h_idx, w_idx) (h_idx * width * 3) + (w_idx * 3) + 0
-#define GREEN(h_idx, w_idx) (h_idx * width * 3) + (w_idx * 3) + 1
-#define BLUE(h_idx, w_idx) (h_idx * width * 3) + (w_idx * 3) + 2
+#define IMG_INDEX(w_idx, h_idx) (h_idx * width) + w_idx
+#define RED(w_idx, h_idx) (h_idx * width * 3) + (w_idx * 3) + 0
+#define GREEN(w_idx, h_idx) (h_idx * width * 3) + (w_idx * 3) + 1
+#define BLUE(w_idx, h_idx) (h_idx * width * 3) + (w_idx * 3) + 2
 
-double S_x[3][3] = {{-1.0, 0.0, 1.0}, {-2.0, 0.0, 2.0}, {-1.0, 0.0, 1.0}};
-double S_y[3][3] = {{-1.0, -2.0, -1.0}, {0.0, 0.0, 0.0}, {1.0, 2.0, 1.0}};
+double S_x[3][3] = {{1.0, 0.0, -1.0}, {2.0, 0.0, -2.0}, {1.0, 0.0, -1.0}};
+double S_y[3][3] = {{1.0, 2.0, 1.0}, {0.0, 0.0, 0.0}, {-1.0, -2.0, -1.0}};
 
 /**
  * `in_buffer` is rgb image with `height` and `width`
@@ -24,31 +23,32 @@ double S_y[3][3] = {{-1.0, -2.0, -1.0}, {0.0, 0.0, 0.0}, {1.0, 2.0, 1.0}};
  */
 void sobel(unsigned char *in_buffer, size_t width, size_t height,
            unsigned char *out_buffer) {
-    for (unsigned int height_idx = 0; height_idx < height - 2; ++height_idx) {
-        for (unsigned int width_idx = 0; width_idx < width - 2; ++width_idx) {
+    for (unsigned int width_idx = 0; width_idx < width - 0; ++width_idx) {
+        for (unsigned int height_idx = 0; height_idx < height - 0;
+             ++height_idx) {
             double G_x =
-                (S_x[0][0] * in_buffer[RED(height_idx, width_idx)]) +
-                (S_x[0][1] * in_buffer[RED(height_idx, width_idx + 1)]) +
-                (S_x[0][2] * in_buffer[RED(height_idx, width_idx + 2)]) +
-                (S_x[1][0] * in_buffer[RED(height_idx + 1, width_idx)]) +
-                (S_x[1][1] * in_buffer[RED(height_idx + 1, width_idx + 1)]) +
-                (S_x[1][2] * in_buffer[RED(height_idx + 1, width_idx + 2)]) +
-                (S_x[2][0] * in_buffer[RED(height_idx + 2, width_idx)]) +
-                (S_x[2][1] * in_buffer[RED(height_idx + 2, width_idx + 1)]) +
-                (S_x[2][2] * in_buffer[RED(height_idx + 2, width_idx + 2)]);
+                (S_x[0][0] * in_buffer[RED(width_idx, height_idx)]) +
+                (S_x[0][1] * in_buffer[RED(width_idx, height_idx + 1)]) +
+                (S_x[0][2] * in_buffer[RED(width_idx, height_idx + 2)]) +
+                (S_x[1][0] * in_buffer[RED(width_idx + 1, height_idx)]) +
+                (S_x[1][1] * in_buffer[RED(width_idx + 1, height_idx + 1)]) +
+                (S_x[1][2] * in_buffer[RED(width_idx + 1, height_idx + 2)]) +
+                (S_x[2][0] * in_buffer[RED(width_idx + 2, height_idx)]) +
+                (S_x[2][1] * in_buffer[RED(width_idx + 2, height_idx + 1)]) +
+                (S_x[2][2] * in_buffer[RED(width_idx + 2, height_idx + 2)]);
 
             double G_y =
-                (S_y[0][0] * in_buffer[RED(height_idx, width_idx)]) +
-                (S_y[0][1] * in_buffer[RED(height_idx, width_idx + 1)]) +
-                (S_y[0][2] * in_buffer[RED(height_idx, width_idx + 2)]) +
-                (S_y[1][0] * in_buffer[RED(height_idx + 1, width_idx)]) +
-                (S_y[1][1] * in_buffer[RED(height_idx + 1, width_idx + 1)]) +
-                (S_y[1][2] * in_buffer[RED(height_idx + 1, width_idx + 2)]) +
-                (S_y[2][0] * in_buffer[RED(height_idx + 2, width_idx)]) +
-                (S_y[2][1] * in_buffer[RED(height_idx + 2, width_idx + 1)]) +
-                (S_y[2][2] * in_buffer[RED(height_idx + 2, width_idx + 2)]);
+                (S_y[0][0] * in_buffer[RED(width_idx, height_idx)]) +
+                (S_y[0][1] * in_buffer[RED(width_idx, height_idx + 1)]) +
+                (S_y[0][2] * in_buffer[RED(width_idx, height_idx + 2)]) +
+                (S_y[1][0] * in_buffer[RED(width_idx + 1, height_idx)]) +
+                (S_y[1][1] * in_buffer[RED(width_idx + 1, height_idx + 1)]) +
+                (S_y[1][2] * in_buffer[RED(width_idx + 1, height_idx + 2)]) +
+                (S_y[2][0] * in_buffer[RED(width_idx + 2, height_idx)]) +
+                (S_y[2][1] * in_buffer[RED(width_idx + 2, height_idx + 1)]) +
+                (S_y[2][2] * in_buffer[RED(width_idx + 2, height_idx + 2)]);
 
-            out_buffer[(height_idx * width) + width_idx] =
+            out_buffer[IMG_INDEX(width_idx, height_idx)] =
                 (unsigned char)sqrt((G_x * G_x) + (G_y * G_y));
         }
     }
@@ -62,21 +62,54 @@ void write_ppm_file(char *path, unsigned char *img_buffer, unsigned int width,
     const char *comment = "# image with sobel filter applied";
     fprintf(fp, "P5\n %s\n %d %d\n %d\n", comment, width, height, 255);
     fwrite(img_buffer, width * height, 1, fp);
+    if (ferror(fp)) {
+        perror("Error while writing output file");
+    }
     fclose(fp);
 }
 #endif
 
 #ifdef ENABLE_LOCAL_MAIN
-int main() {
+int main(int argc, char *argv[]) {
     size_t width = 128;
     size_t height = 85;
+    char *in_path = "eisvogel.data";
+    char *out_path = "./sobel.ppm";
+    if (argc == 4) {
+        width = atoi(argv[1]);
+        height = atoi(argv[2]);
+        in_path = argv[3];
+        out_path = argv[4];
+    }
 
-    unsigned char greyscale_image[height * width];
+    unsigned char final_image[width * height];
 
-    sobel(eisvogel_data, width, height, greyscale_image);
+    unsigned char img[width * height * 3];
+
+    printf("Reading input file...\n");
+    FILE *fp;
+    fp = fopen(in_path, "rb");
+    for (size_t idx = 0; idx < height; ++idx) {
+        unsigned char img_line[width];
+        fread(img_line, sizeof(unsigned char), width * 3, fp);
+        if (ferror(fp)) {
+            perror("Error while reading input file");
+            break;
+        }
+        memcpy(&img[idx * width * 3], &img_line,
+               sizeof(unsigned char) * width * 3);
+    }
+    fclose(fp);
+    printf("Read input file.\n");
+
+    printf("Compute sobel filter...\n");
+    sobel(img, width, height, final_image);
+    printf("Computed sobel filter.\n");
 
 #ifdef DEBUG
-    write_ppm_file("./sobel.ppm", greyscale_image, width, height);
+    printf("Writing output file...\n");
+    write_ppm_file(out_path, final_image, width, height);
+    printf("Wrote output file.\n");
 #endif
 
     return EXIT_SUCCESS;
